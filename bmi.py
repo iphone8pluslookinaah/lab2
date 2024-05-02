15ce290209116655b96f996eda5e041b8c7e7001
Kai <kaiweng.23@ichat.sp.edu.sg>
2024-05-02 10:39:29 +0800

    updt bmi.py

--- bmi.py
+++ bmi.py
@@ -1,24 +1,22 @@
 import math
 print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
 
-def funcName(parameter1, parameter2):
-    print("dummy funct")
-    return 10
-
 def bmi_range(bmi):
     if (bmi < 18.5):
         print("under weight")
+        return -1
     elif (bmi > 25.0):
         print("over weight")
+        return 0
     else:
         print("normal weight")
-    return
+        return 1
 
 def calc_bmi(height, weight):
     print("Height = ", height)
     print("Weight = ", weight)
     bmi = weight / pow(height, 2)
     print("bmi =", bmi)
-    bmi_range(bmi=bmi)
+    bmi_range(bmi)
 
 calc_bmi(weight=57, height=1.73)
